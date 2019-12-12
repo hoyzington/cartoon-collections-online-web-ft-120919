a26bec6008f88d0dd676d3042ff69020636150c5
Scott Hoisington <hoyzington@gmail.com>
2019-12-12 12:42:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(elements)
   calls = []
-  elements.map {|element| calls << element.upcase}
+  elements.map {|element| calls << element.capitalize + "!"}
   calls
 end
 
